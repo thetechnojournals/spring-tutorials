aef9cedf87348f3b67f26b48fd50c2326f3fb87b
thetechnojournals <thetechnojournals@gmail.com>
2019-12-09 20:41:51 +0530

    Adding data flow server to git.

---
+++ SpringCloudDataFlowServer/src/test/java/com/ttj/dataflow/MainDataFlowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttj.dataflow;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainDataFlowServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
